ee079a85888f9147fcc674b990c1a468a78c6e61
fabezio <fabezio@outlook.fr>
2020-05-03 23:17:08 +0200

    ajout methode de gestion diurne/nocturne de theme

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
-v-app-bar(app dark flat tile)
+v-app-bar(app dark="handleTheme" flat tile)
   v-toolbar(flat padless tile).py-n2
     Brand(label='Fabezio')
     Navlinks
     Navlinks(url='skills', label='Compétences' icon='text-box-check')
     Navlinks(url='contact', label='Contactez-moi!' icon='pen')
     v-spacer
-    <v-btn @click="toggleTheme" color="info">{{theme}}</v-btn>
+    v-btn(@click='toggleTheme', color='info') {{theme}}
+    v-chip(v-if="" @click="showClock")
+      v-avatar
+        v-icon mdi-clock
+    v-chip(@click="showClock") {{hour}}:{{min}}
 
 
 </template>
@@ -15,11 +19,17 @@ v-app-bar(app dark flat tile)
 export default {
   data() {
     return {
-      // dark: this.$vuetify.theme.dark
-      theme: 'sombre'
+      theme: 'sombre',
+      min: new Date().getMinutes(),
+      hour: new Date().getHours()
     }
   },
   methods: {
+    handleTheme() {
+      this.hour >= 6 && this.hour < 22
+        ? (this.$vuetify.theme.dark = false)
+        : (this.$vuetify.theme.dark = true)
+    },
     toggleTheme() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark
       !this.$vuetify.theme.dark
